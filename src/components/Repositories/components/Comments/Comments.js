e709c7b6cf2716e48db9eb677703defc53d487c1
marcos8896 <elyomarcos@gmail.com>
2018-12-05 02:30:27 -0600

    Add Comments component

---
+++ src/components/Repositories/components/Comments/Comments.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+
+import PropTypes from 'prop-types'; 
+
+import './Comments.scss';
+
+const Comments = ({ items }) => {
+
+  let commentElements = null;
+
+  if( items !== undefined )
+    commentElements = items.map(comment => (
+      <ListItem key={comment.id} alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt={comment.user.login} src={comment.user.avatar_url} />
+        </ListItemAvatar>
+        <ListItemText
+          secondary={
+            <React.Fragment>
+              <span className="commit-id">
+                {`Commit: ${comment.commit_id}`}
+              </span>
+              <Typography component="span" className="inline-comments" color="textPrimary">
+                {comment.body}
+              </Typography>
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    ))
+
+  return (
+    <List component="div" disablePadding>
+      {commentElements}
+    </List>
+  );
+}
+
+Comments.propTypes = {
+  items: PropTypes.array,
+};
+
+
+export default Comments;
