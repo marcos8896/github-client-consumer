5dca753ae6c57ac84d9303f65b6c03a2fc52d15c
marcos8896 <elyomarcos@gmail.com>
2018-12-04 21:27:40 -0600

    Add axios to the project

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from './App';
 
-import { render, waitForElement } from 'react-testing-library';
+import { render } from 'react-testing-library';
 
 it('renders without crashing with react testing library', () => {
   const { getByText } = render(<App/>);
