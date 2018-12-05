fd6a45f12df4750d6d4e4ede3e780cdc867c8349
marcos8896 <elyomarcos@gmail.com>
2018-12-04 20:55:15 -0600

    Setup testing environment correctly

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { render, waitForElement } from 'react-testing-library';
+
+it('renders without crashing with react testing library', () => {
+  const { getByText } = render(<App/>);
+  expect( getByText( 'Learn React' ) ).toBeInTheDocument();
 });
---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+
+// add some helpful assertions
+import 'jest-dom/extend-expect'
+
+// this is basically: afterEach(cleanup)
+import 'react-testing-library/cleanup-after-each'
